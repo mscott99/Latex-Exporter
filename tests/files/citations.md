6bc486972d68e49be43deab596fc90dc438f2696
Matthew Scott <m.valckescott@gmail.com>
2024-09-05 16:55:00 -0700

    Add tests for inline and citations.

---
+++ tests/files/citations.md
@@ -0,0 +1,1 @@
+I cite [[@first]], [std][[@second]], [txt][[@first]], [[@first]][[@second]], [Remark 1][[@second]].
--- tests/inline.test.ts
+++ tests/inline.test.ts
@@ -13,6 +13,18 @@ describe("all inline", () => {
 		);
 		expect(result)
 			.toEqual(`A \\emph{emph} \\textbf{strong} \`\`quotes" \`single'
+Another \\emph{emph} \\textbf{strong}
+`);
+	});
+	test("citation", async () => {
+		let settings = DEFAULT_SETTINGS;
+		settings.default_citation_command = "othercite"
+		const result = await get_latex_file_contents(
+			"citations",
+			settings,
+		);
+		expect(result)
+			.toEqual(`I cite \\othercite{first}, \\cite{second}, \\textcite{first}, \\cite{first, second}, \\cite[Remark 1]{second}.
 `);
 	});
 });
