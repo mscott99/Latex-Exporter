b704ae47c0f43631a5a34efac7e213dc82f7a269
Matthew Scott <m.valckescott@gmail.com>
2026-03-27 17:45:06 -0700

    Fix broken comment test

--- tests/display.test.ts
+++ tests/display.test.ts
@@ -4,6 +4,8 @@ import { DEFAULT_SETTINGS } from "../src/export_longform/interfaces";
 
 describe("my plugin", () => {
 	test("Quote", async () => {
+		const settings = DEFAULT_SETTINGS
+		settings.export_comments = true
 		const result = await get_latex_file_contents("quote", DEFAULT_SETTINGS);
 		expect(result).toEqual(`Hi I speak
 % And here is a quote
